14002feb85913e32d2d9bb3e38585d6fa8fa6ad4
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-06 17:49:19 +0530

    update

--- Student Directory/script.js
+++ Student Directory/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   loadStudents();
-
   async function loadStudents() {
     $("#studentList").html(
       '<div class="col-12 text-center"><h5 class="text-white">Loading...</h5></div>'
@@ -20,14 +19,14 @@ $(document).ready(function () {
 
   // Render student cards
 
-  function renderStudents(studens) {
+  function renderStudents(students) {
     $("#studentList").empty();
 
-    studens.forEach((student) => {
+    students.forEach((student) => {
       $("#studentList").append(`<div class="col-sm-6 col-md-4">
-                                  <div class="card student-card h-100 p-3">
-                                  <div class="card-body">
-                                  <h5 class="card-title">${student.firstname} ${student.lastname}</h5>
+                                  <div class="card student-card h-100 p-3 bg-primary">
+                                  <div class="card-body bg-white rounded">
+                                  <h5 class="card-title">${student.firstName} ${student.lastName}</h5>
                                   <p class="card-text text-muted small mb-0">${student.email}</p>
                                                           </div>
                                                     </div>
@@ -36,11 +35,12 @@ $(document).ready(function () {
     });
   }
 
+
   // Search filter
   $("#searchInput").on("keyup", function () {
     const q = $(this).val().toLowerCase();
 
-    $("#studentList col-sm-6").each(function () {
+    $("#studentList .col-sm-6").each(function () {
       const text = $(this).text().toLowerCase();
       $(this).toggle(text.includes(q));
     });
@@ -50,5 +50,4 @@ $(document).ready(function () {
   $("#clearSearch").click(function () {
     $("#searchInput").val("").trigger("keyup");
   });
-  
 });
